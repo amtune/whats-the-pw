1869d5ff44f30737a894d63a79edee0e419c1e92
amtune <allisontune@gmail.com>
2023-08-06 19:56:33 -0400

    console log working

--- Develop/script.js
+++ Develop/script.js
@@ -1,24 +1,31 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var toUpper = function(x) {  // found this code snippet, gonna try to use. confusing but it works" probably wont remember it.
-  return x.toUpperCase();
-};
+function generateRandomPassword(length) {
+  const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+[]{}|;:,.<>?";
+  let password = "";
 
-var specialChars = ["!", "#", "$", "%", "&", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?"];
-var numericChars = ["1", "2", "3", "4", "5", "6", "7","8","9"];
-var lowerCaseChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s","t","u","v","w","x","y","z" ];
-var upperCaseChars = lowerCaseChars.map(toUpper); 
- console.log(upperCaseChars);
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * charset.length);
+    password += charset.charAt(randomIndex);
+  }
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+  return password;
 }
 
+const password = generateRandomPassword(12); // Change the length as needed
+console.log(password);
+
+// Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// }
+
+
+
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", generateRandomPassword);
